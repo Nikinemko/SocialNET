61ce751a4870acd8ad388043b62416436f0ca0cd
Nikinemko <Nikinemko@gmail.com>
2024-01-17 17:48:38 +0200

    setting up firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
+import { environment } from 'src/environment/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    FirebaseTSApp.init(environment.firebaseConfig);
+  }
+}
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyB--sRrA9GiHy4fpYa7Tlrf6JfHrL8gIaU',
+    authDomain: 'socialnetwork-c8a0f.firebaseapp.com',
+    projectId: 'socialnetwork-c8a0f',
+    storageBucket: 'socialnetwork-c8a0f.appspot.com',
+    messagingSenderId: '956740177659',
+    appId: '1:956740177659:web:a27f50995141c62a642a5e',
+    measurementId: 'G-FLCB56YL79',
+  },
+};
